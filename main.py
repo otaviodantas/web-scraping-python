b018680a9f82a57049f3996768f8ecb8e4c0ce8a
Otávio Mendonça <otavio-dantas@live.com>
2020-05-04 23:16:42 -0300

    Alteration

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 
-url_input = 'https://matchhistory.br.leagueoflegends.com/pt/#match-history/BR1/204720835' 
-request   = urlopen(url_input)
-only_html = request.read()
+try:
+    url_input = 'https://matchhistory.br.leagueoflegends.com/pt/#match-history/BR1/204720835' 
+    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
+    request   = Request(url_input, headers = header)
+    response  = urlopen(request)
+    html      = response.read()
 
-soup = BeautifulSoup(only_html, 'html.parser')
-soup = soup.find ('span', {'class' : "map-mode-mode"}, id = "binding-7713")
-print(soup)
+except HTTPError as e:
+    print(e.status, e.reason) 
+
+except URLError as e:
+    print(e.reason)
+
+html = html.decode('utf-8')
+html = " ".join(html.split()).replace('> <', '><')
+
+soup = BeautifulSoup(html, 'html.parser')
+seach = soup.findAll('div', id = 'main')
+print(seach)
+
