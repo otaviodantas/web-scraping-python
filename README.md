1f69bc6190e761b7d67b1788bea5fb39b95f636b
Otávio Mendonça <otavio-dantas@live.com>
2020-06-09 14:32:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# WEB SCRAPING
+
+## Introdução
+
+O método de web scraping consiste em obter dados de uma página web, navegando pelo seu código HTML e capturando os dados que interassam, para posteriormente usa-los em outras aplicações.
+
+Um exemplo da utilização dessa ferramenta pode ser vista quando vamos até um site de vendas de algum produto, e queremos prever o preço desse produto daqui a alguns dias ou meses. Para isso, aplicamos o web scraping para obter os dados dos preços que estão sendo mostrados no site.
--- main.py
+++ main.py
@@ -2,23 +2,33 @@ from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 
-try:
-    url_input = 'https://matchhistory.br.leagueoflegends.com/pt/#match-history/BR1/204720835' 
-    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
-    request   = Request(url_input, headers = header)
-    response  = urlopen(request)
-    html      = response.read()
+url_input = 'https://alura-site-scraping.herokuapp.com/hello-world.php'
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
-except HTTPError as e:
-    print(e.status, e.reason) 
+response = urlopen(url=url_input)
+html_only = response.read()
 
-except URLError as e:
-    print(e.reason)
+soup = BeautifulSoup(html_only, 'html.parser')
+find = soup.find('h1', id="hello-world").get_text()
 
-html = html.decode('utf-8')
-html = " ".join(html.split()).replace('> <', '><')
+print(find)
 
-soup = BeautifulSoup(html, 'html.parser')
-seach = soup.findAll('div', id = 'main')
-print(seach)
 
+# try:
+#     request   = Request(url_input, headers = header)
+#     response  = urlopen(request)
+
+# except HTTPError as e:
+#     print(e.status, e.reason)
+
+# except URLError as e:
+#     print(e.reason)
+
+# html = response.read()
+# html = html.decode('utf-8')
+# html = " ".join(html.split()).replace('> <', '><')
+
+# soup = BeautifulSoup(html, 'html.parser')
+# seach = soup.find('div')
+# print(seach)
