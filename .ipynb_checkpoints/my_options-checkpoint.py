a1089ac719d8c65461f867556ba9d9f12484b739
Otávio Mendonça <otavio-dantas@live.com>
2020-06-13 03:41:09 -0300

    upgrades

--- .ipynb_checkpoints/my_options-checkpoint.py
+++
@@ -1,4 +0,0 @@
-URL_INPUT = 'https://alura-site-scraping.herokuapp.com/index.php'
-
-HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
